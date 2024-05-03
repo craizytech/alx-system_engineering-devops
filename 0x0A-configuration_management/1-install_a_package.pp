9af4798f2f4d6311a762cd3b174abb2cf3d1df33
craizytech <craizytech@gmail.com>
2024-05-03 22:21:12 +0300

    fixed a package installation using puppet'

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # Installing a flask application from pip3 in a puppet
-exec { 'install flask':
-  command => 'pip3 install flask==2.1.0',
-  path    => 'usr/bin'
-  unless  => 'pip3 list | grep flask'
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
