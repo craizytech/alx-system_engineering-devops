e8e338f6e3240c4e811e7189cc1811f7d81bafa9
craizytech <craizytech@gmail.com>
2024-08-11 09:27:38 +0300

    created a puppet file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # This file creates a file school and sets its permissions
 file {'/tmp/school':
-	path => '/tmp/school',
-	ensure => present,
-	owner => 'www-data',
-	group => 'www-data',
-	mode => '0744',
-	content => 'I love Puppet'
+    ensure  => present,
+    path    => '/tmp/school',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet'
 }
