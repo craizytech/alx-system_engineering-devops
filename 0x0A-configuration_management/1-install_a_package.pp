428330266bf5e0534c13daa3956b6eda387ad512
craizytech <craizytech@gmail.com>
2024-08-11 09:55:49 +0300

    wrote a puppet script that installs flask 2.1.0 with pip

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,13 @@
+#This puppet manifest installs flask version 2.1.0
+
+# ensure that pip ins installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# ensure flask version 2.1.0 is installed via pip3
+package { 'flask':
+  ensure   => installed,
+  provider => 'pip3',
+  require  => Package['python3-pip']
+}
