0187bdb38e0b733a47b28e1e6bcc28e99cf8cca5
craizytech <craizytech@gmail.com>
2024-08-18 22:05:59 +0300

    modified the config file

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,47 +1,56 @@
-# add stable version of nginx
-exec { 'add nginx stable repo':
-  command => 'sudo add-apt-repository ppa:nginx/stable',
+# Add stable version of Nginx
+exec { 'add_nginx_stable_repo':
+  command => 'add-apt-repository -y ppa:nginx/stable',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  unless  => 'grep -q "nginx/stable" /etc/apt/sources.list /etc/apt/sources.list.d/*',
 }
 
-# update software packages list
-exec { 'update packages':
+# Update software packages list
+exec { 'update_packages':
   command => 'apt-get update',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  require => Exec['add_nginx_stable_repo'],
 }
 
-# install nginx
+# Install Nginx
 package { 'nginx':
-  ensure     => 'installed',
+  ensure  => 'installed',
+  require => Exec['update_packages'],
 }
 
-# allow HTTP
-exec { 'allow HTTP':
+# Allow HTTP through UFW
+exec { 'allow_http':
   command => "ufw allow 'Nginx HTTP'",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  onlyif  => '! dpkg -l nginx | egrep \'îi.*nginx\' > /dev/null 2>&1',
+  unless  => "ufw status | grep -q 'Nginx HTTP'",
 }
 
-# change folder rights
-exec { 'chmod www folder':
-  command => 'chmod -R 755 /var/www',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+# Set correct folder permissions
+file { '/var/www/html':
+  ensure  => directory,
+  mode    => '0755',
+  recurse => true,
 }
 
-# create index file
+# Create index file
 file { '/var/www/html/index.html':
   content => "Hello World!\n",
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0644',
 }
 
-# create index file
+# Create custom 404 error page
 file { '/var/www/html/404.html':
   content => "Ceci n'est pas une page\n",
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0644',
 }
 
-# add redirection and error page
-file { 'Nginx default config file':
+# Manage Nginx default config file
+file { '/etc/nginx/sites-enabled/default':
   ensure  => file,
-  path    => '/etc/nginx/sites-enabled/default',
   content =>
 "server {
 	listen 80 default_server;
@@ -49,14 +58,14 @@ file { 'Nginx default config file':
 
 	root /var/www/html;
 	index index.html;
-	
+
 	server_name _;
-	
+
 	location /redirect_me {
 		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
 	}
-  
-  add_header X-Served-By \$hostname;
+
+	add_header X-Served-By \$hostname;
 
 	location / {
 		try_files \$uri \$uri/ =404;
@@ -69,15 +78,13 @@ file { 'Nginx default config file':
 	}
 }
 ",
-}
-# restart nginx
-exec { 'restart service':
-  command => 'service nginx restart',
-  path    => '/usr/bin:/usr/sbin:/bin',
+  notify  => Service['nginx'],
 }
 
-# start service nginx
+
+# Start and enable Nginx service
 service { 'nginx':
   ensure  => running,
+  enable  => true,
   require => Package['nginx'],
 }
